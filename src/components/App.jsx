1d47bd0d0c5c4796b8d2e3e4ddc82eec09fd7b00
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2021-11-20 19:52:57 +0800

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+
+function App(){
+    return(
+        <div>
+            <Header />
+            <Content />
+        </div>
+    );
+}
+
+export default App;
