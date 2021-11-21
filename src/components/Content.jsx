6b7181fa1f4a761fbb11173c022cd05aa3fa8f42
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2021-11-21 21:26:53 +0800

    Add Header and Content (with Grid)

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,7 +2,24 @@ import React from "react";
 
 function Content(){
     return(
-        <div>
+        <div id="content-section">
+
+        <div className="columns">
+
+        <div className="left-pane">
+                <p>LEFT Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                In venenatis leo sit amet turpis porttitor, nec rhoncus arcu vulputate. 
+                Quisque fringilla consequat eros eget tempor. Mauris fermentum, risus ac iaculis vestibulum, magna justo sagittis elit, quis consequat quam neque eu urna. Proin hendrerit in dui ornare finibus. Etiam posuere mollis risus. Vivamus dignissim hendrerit metus ac interdum. Curabitur sed orci magna. Quisque ac purus suscipit, ullamcorper orci at, commodo elit.
+                </p>
+            </div>
+
+            <div className="right-pane">
+                <p>RIGHT Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                In venenatis leo sit amet turpis porttitor, nec rhoncus arcu vulputate. 
+                Quisque fringilla consequat eros eget tempor. Mauris fermentum, risus ac iaculis vestibulum, magna justo sagittis elit, quis consequat quam neque eu urna. Proin hendrerit in dui ornare finibus. Etiam posuere mollis risus. Vivamus dignissim hendrerit metus ac interdum. Curabitur sed orci magna. Quisque ac purus suscipit, ullamcorper orci at, commodo elit.
+                </p>
+            </div>
+        </div>
 
         </div>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 function Header(){
     return(
-        <div>
-            This is Header.
+        <div id="header-section">
+            <div className="title-picture">
+                <img src="https://picsum.photos/2100/900" alt="title" />
+            </div>
+
+                <div className="title-section">
+                    <img className="title" src="images/taigu-logo-placeholder-white.png" alt="logo" />
+                    <h3 className="slogan">
+                    專業字畫修復裝裱
+                    </h3>
+                </div>
         </div>
     )
 }
