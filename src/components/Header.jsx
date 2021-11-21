73a4bddb8724da4a3ab410e4b98b698b6338b76c
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2021-11-21 22:22:03 +0800

    Experiment with Flex on Header Title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,13 @@ function Header(){
     return(
         <div id="header-section">
             <div className="title-picture">
-                <img src="https://picsum.photos/2100/900" alt="title" />
+                <img src="https://picsum.photos/1050/300" alt="title" />
             </div>
 
-                <div className="title-section">
+                <div className="title-flexbox">
+                <div className="title-image">
                     <img className="title" src="images/taigu-logo-placeholder-white.png" alt="logo" />
+                </div>
                     <h3 className="slogan">
                     專業字畫修復裝裱
                     </h3>
