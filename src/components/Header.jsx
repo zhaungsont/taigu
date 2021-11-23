7bc6271bf535d584830e4bf73b234a053b9680c2
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2021-11-24 01:52:28 +0800

    Restructure Styles in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,12 @@ function Header(){
     return(
         <div id="header-section">
             <div className="title-picture">
-                <img src="https://picsum.photos/1050/300" alt="title" />
-            </div>
-
-                <div className="title-flexbox">
-                <div className="title-image">
-                    <img className="title" src="images/taigu-logo-placeholder-white.png" alt="logo" />
-                </div>
-                    <h3 className="slogan">
-                    專業字畫修復裝裱
+                <img className="cover-img" src="images/boss4.jpg" alt="title" />
+                <img className="logo" src="images/taigu-logo-placeholder-white.png" alt="太古齋" />
+                <h3 className="slogan">
+                    專 業 字 畫 修 復 裝 裱
                     </h3>
-                </div>
+            </div>              
         </div>
     )
 }
