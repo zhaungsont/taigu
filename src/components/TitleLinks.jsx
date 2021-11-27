610448475c1db88d467433f24e6be1b6a223c35d
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2021-11-28 03:35:23 +0800

    Add GetDate() to TitleLinks.jsx

--- src/components/TitleLinks.jsx
+++ src/components/TitleLinks.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=Taipei&appid=" + process.env.WEATHER_API + "&units=metric&lang=zh_tw";
+
+
+let today = new Date();
+
+let options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+};
+let day = today.toLocaleString("zh-TW", options);
+
+
 const sp = " ";
 
 function TitleLinks(){
     return(
         <div className="title-links">
-            <a href="https://goo.gl/maps/vkNBMjAm7jaSzB8m7"><i class="fas fa-map-marker-alt"></i></a> {sp}
-            <a href="https://www.facebook.com/taigu168/"><i class="fab fa-facebook"></i></a> {sp}
-            <a href="https://github.com/zhaungsont"><i class="fab fa-github"></i></a>
-
+            <a href="https://goo.gl/maps/vkNBMjAm7jaSzB8m7"><i className="fas fa-map-marker-alt"></i></a> {sp}
+            <a href="https://www.facebook.com/taigu168/"><i className="fab fa-facebook"></i></a> {sp}
+            <a href="https://github.com/zhaungsont"><i className="fab fa-github"></i></a>
+            <p>{sp}{day}</p>
             <hr />
+
         </div>
     )
 }
