0cc44a78120038010625398cd1b119399c45327a
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2021-11-28 17:21:12 +0800

    Add Fonts

--- src/components/CarouselGallery.jsx
+++ src/components/CarouselGallery.jsx
@@ -5,7 +5,15 @@ import { Carousel } from 'react-responsive-carousel';
 
 function CarouselGallery(){
     return (
-        <Carousel showArrows={true} autoPlay={true} interval={3000} swipeable={true} emulateTouch={true} infiniteLoop={true} showThumbs={false} showStatus={false} >
+        <Carousel 
+        showArrows={true} 
+        autoPlay={true} 
+        interval={3000} 
+        swipeable={true} 
+        emulateTouch={true} 
+        infiniteLoop={true} 
+        showThumbs={false} 
+        showStatus={false} >
         
         <div>
             <img src="images/pic4.jpg" />
@@ -17,10 +25,10 @@ function CarouselGallery(){
             <img src="images/taigu2.jpg" />
             
         </div>
-        <div>
+        {/* <div>
             <img src="images/boss4.jpg" />
             
-        </div>
+        </div> */}
         <div>
             <img src="images/boss2.jpg" />
         </div>
